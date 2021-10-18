d16487e9467644cc936ee83c09bec00047e3960e
MinhajSadik <MinhajoSadik@gmail.com>
2021-10-18 19:52:43 +0600

    [add] some code c/c++

---
+++ Beginning/C-Practice/print.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("hello '\teveryone'\n");
+    printf("i'm 'Minhaj Sadik'\n");
+    printf("i'm a alone programmer'\n");
+    printf("but now i'm working with a company that's name is Ajax Business solutions\n");
+    printf("i'm not properly and organaized 'programmer!'\n");
+
+    printf("%d\n", 5 + 10);
+    printf("%d\n", 5 - 10);
+    printf("%d\n", 5 * 10);
+    printf("%d\n", 5 / 10);
+    
+    return 0;
+}
