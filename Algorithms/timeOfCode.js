138af2b745895464bdc7922e41d1d78c2f0e9f81
MinhajSadik <MinhajoSadik@gmail.com>
2022-06-17 14:50:39 +0600

    [ADD] learned BigO notation extra something

--- Algorithms/timeOfCode.js
+++ Algorithms/timeOfCode.js
@@ -1,3 +1,6 @@
+//Language: JavaScript
+//Path: Algorithms/timeOfCode.js
+//Time Complexity: BigO(n)
 function sumOfCode(n) {
   let sum = 0;
   for (let i = 0; i <= n; i++) {
@@ -8,8 +11,23 @@ function sumOfCode(n) {
 
 console.log(sumOfCode(10));
 
+//Time Complexity: BigO(1)
 function sumOfCode1(n) {
   return (n * (n + 1)) / 2;
 }
 
 console.log(sumOfCode1(10));
+
+//Time Complexity: BigO(n^2)
+function multipleOfCode(n) {
+  let sum = 0;
+  for (let i = 0; i <= n; i++) {
+    for (let j = 0; j <= n; j++) {
+      sum += i * j;
+      console.log(i, j);
+    }
+  }
+  return sum;
+}
+
+console.log(multipleOfCode(5));
