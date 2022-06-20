2246405ca46fd74d9c44ae81c900e8231265c8e5
MinhajSadik <MinhajoSadik@gmail.com>
2022-06-21 00:00:08 +0600

    [ADD] trying to learn logarithm

---
+++ Algorithms/logarithm.js
@@ -0,0 +1,21 @@
+//Language: JavaScript
+//Path: Algorithms/logarithm.js
+
+function logarithm(n) {
+  while (n > 1) {
+    n /= 2;
+    console.log(n);
+  }
+  return n;
+}
+
+console.log(logarithm(16));
+
+function anotherLogarithm(n) {
+  if (n <= 1) {
+    return n;
+  }
+  return anotherLogarithm(n / 2);
+}
+
+console.log(anotherLogarithm(16));
