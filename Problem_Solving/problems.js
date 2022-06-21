a6df21c12eb9682958bce6338c3efa756876650c
MinhajSadik <MinhajoSadik@gmail.com>
2022-06-21 22:19:31 +0600

    [ADD] problem_3:

--- Problem_Solving/problems.js
+++ Problem_Solving/problems.js
@@ -1,6 +1,7 @@
 //Language: JavaScript
 //Path: Problem_Solving/problems.js
 
+//Problem 1:
 function getHigherAndLower(arr) {
   let higher = arr[0];
   let lower = arr[0];
@@ -18,3 +19,40 @@ function getHigherAndLower(arr) {
 }
 
 console.log(getHigherAndLower([-4, 5, "emit", -9, 11, -3]));
+
+//Problem 2:
+function charCount(str) {
+  let countedChar = {};
+  for (let i = 0; i < str.length; i++) {
+    //remove white space and extra symbols
+    let currentChar = str[i].replace(/[^a-zA-Z]/g, "");
+    if (currentChar === "") continue;
+
+    let char = str[i].toLowerCase();
+    if (countedChar[char]) {
+      countedChar[char]++;
+    } else {
+      countedChar[char] = 1;
+    }
+  }
+  return countedChar;
+}
+
+console.log(charCount("hello Everyone! @@  ##"));
+
+//Problem 3:
+function sameFrequency(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    const currentIndex = arr2.indexOf(arr1[i] ** 2);
+    if (currentIndex === -1) {
+      return false;
+    }
+    arr2.splice(currentIndex, 1);
+  }
+  return true;
+}
+
+console.log(sameFrequency([2, 3, 4], [9, 4, 16]));
